2ede7173a964bc502115fb2c696822a29de19f83
Arie Gerritse <gerritse.arie@gmail.com>
2017-09-14 22:28:16 +0000

    User Registration

--- express_server.js
+++ express_server.js
@@ -14,19 +14,37 @@ app.use(bodyParser.urlencoded({
 
 app.set("view engine", "ejs");
 
-var urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const users = {
+  '9hs5xK': {
+    id: '9hs5xK',
+    email: 'makebeleive@gmail.com',
+    password: 'password'
+  },
+  'Vn2b2x': {
+    id: 'Vn2b2x',
+    email: 'gmail@gmail.com',
+    password: 'password'
+  }
 };
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.get("/", (req, res) => {
+  res.render("urls_test");
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    username: req.cookies.username
+    usersInfo: users,
+    user_id: req.cookies.user_id
   };
   let randomString = generateRandomString();
   var linkName = "Go to your link!";
@@ -35,14 +53,42 @@ app.get("/urls", (req, res) => {
   }
   res.render("urls_index", templateVars);
 });
-
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/urls');
+  let tempVars = {
+    response: ''
+  };
+
+  if (req.body.password != undefined && req.body.email != undefined && req.body.password != '') {
+    let i = 1;
+    Object.keys(users).forEach(function(element) {
+      if (users[element].email === req.body.email && users[element].password === req.body.password) {
+        res.cookie('user_id', element);
+        res.redirect('/urls');
+        i = 0;
+      }
+      if (Object.keys(users).length === i) {
+        console.log('HERE!');
+        tempVars.response = 'Not a valid username or password';
+        res.render("urls_login", tempVars);
+      }
+      i++;
+    });
+  } else if (req.body.email === '' && req.body.email === '') {
+    tempVars.response = 'Please enter a email and password';
+    res.render("urls_login", tempVars);
+  } else if (req.body.email === '') {
+    tempVars.response = 'Please enter a valid email';
+    res.render("urls_login", tempVars);
+  } else if (req.body.password === '') {
+    tempVars.response = 'Please enter a valid password';
+    res.render("urls_login", tempVars);
+  } else {
+    res.render("urls_login", tempVars);
+  }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
 
@@ -54,10 +100,50 @@ app.get("/urls/new", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
     shortURL: req.params.id,
-    username: req.cookies["username"]
+    usersInfo: users,
+    user_id: req.cookies.user_id
   };
   res.render("urls_new", templateVars);
 });
+
+app.post("/register", (req, res) => {
+  let tempVars = {
+    response: ''
+  };
+
+  if (req.body.password != undefined && req.body.email != undefined && req.body.password != '') {
+    Object.keys(users).forEach(function(element) {
+      if (users[element].email === req.body.email) {
+        console.log(element);
+        console.log(users[element].email + ' ' + req.body.email);
+        tempVars.response = 'That email has already been registered.';
+      }
+    });
+    if (tempVars.response != 'That email has already been registered.') {
+      let randomString = generateRandomString();
+      users[randomString] = {
+        id: randomString,
+        email: req.body.email,
+        password: req.body.password
+      };
+      res.cookie('user_id', randomString);
+      res.redirect('/urls');
+    }
+  } else if (req.body.email === '' && req.body.email === '') {
+    res.render("urls_reg", tempVars);
+  } else if (req.body.email === '') {
+    tempVars.response = 'Please enter a valid email';
+    res.render("urls_reg", tempVars);
+  } else if (req.body.password === '') {
+    tempVars.response = 'Please enter a valid password';
+    res.render("urls_reg", tempVars);
+  } else {
+    res.render("urls_reg", tempVars);
+  }
+
+});
+
+//req.body.password == true && req.body.email == true
 
 app.post("/urls/:id/delete", (req, res) => {
   if (Object.keys(urlDatabase).indexOf(req.params.id) > -1) {
@@ -81,7 +167,8 @@ app.post("/urls/:id", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
     shortURL: req.params.id,
-    username: req.cookies["username"]
+    user_id: req.cookies.user_id,
+    usersInfo: users
   };
   res.render("urls_show", templateVars);
 });
--- test.js
+++ test.js
@@ -1,13 +1,14 @@
-var obj = {
+const obj = {
   name: '12312321',
   name2: '12312321',
   name3: '21321',
   name4: '21312321'
 };
 
-var a = 'name5';
-var b = Object.keys(obj).indexOf(a);
+for (var keys in Object.keys(obj)) {
+  console.log(keys);
+}
 
-console.log(b);
+console.log(obj);
 
 // console.log(Object.keys(obj)[3]);
