ebb00b9ff7248d9da53e6bdde851170a1b7cbc3c
Arie Gerritse <gerritse.arie@gmail.com>
2017-09-14 01:03:51 +0000

    Added cookies

--- express_server.js
+++ express_server.js
@@ -2,6 +2,11 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
+var express = require('express');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -18,43 +23,45 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.post("/urls", (req, res) => {
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies.username
+  };
   let randomString = generateRandomString();
   var linkName = "Go to your link!";
-  urlDatabase[randomString] = req.body.longURL;
-  console.log(req.IncomingMessage);
-  // res.redirect('http://localhost:8080/urls')
-  // res.send(linkName.link(urlDatabase[randomString]));
-
+  if (req.body.longURL) {
+    urlDatabase[randomString] = req.body.longURL;
+  }
+  res.render("urls_index", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = {
-    urls: urlDatabase
-  };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/test", (req, res) => {
-  res.render("urls_test");
-});
-
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    urls: urlDatabase,
+    shortURL: req.params.id,
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   if (Object.keys(urlDatabase).indexOf(req.params.id) > -1) {
     delete urlDatabase[req.params.id];
-
     res.redirect('http://localhost:8080/urls');
   } else {
     res.redirect('http://localhost:8080/urls');
@@ -73,7 +80,8 @@ app.post("/urls/:id/update", (req, res) => {
 app.post("/urls/:id", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    shortURL: req.params.id
+    shortURL: req.params.id,
+    username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
 });
