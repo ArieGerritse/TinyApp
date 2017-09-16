c9544144d42d426d584e1eeb7da5412da6407936
Arie Gerritse <gerritse.arie@gmail.com>
2017-09-16 01:11:26 +0000

    Added a server side URL filter

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 var cookieSession = require('cookie-session');
-
+// All requires for the project
 
 var app = express();
 app.use(cookieParser());
@@ -21,7 +21,8 @@ app.use(cookieSession({
 }));
 
 app.set("view engine", "ejs");
-
+//////////////////////////////////////////////////
+// URL database of both the Short URL, long URL, and the ID of the poster who made the link
 let urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
@@ -32,7 +33,8 @@ let urlDatabase = {
     poster: 'Vn2b2x'
   }
 };
-
+/////////////////////////////////////////////
+//Database of the
 const users = {
   '9hs5xK': {
     id: '9hs5xK',
@@ -54,6 +56,11 @@ const users = {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+//////////////////////////////////////////// DONE
+//look at json information
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 ///////////////////////////////////////////////////////////// DONE
 //Base of tiny app, if logged in redirects to /urls, if not redirects to /login
 app.get("/", (req, res) => {
@@ -63,14 +70,74 @@ app.get("/", (req, res) => {
     res.redirect('/urls');
   }
 });
-///////////////////////////////////////////// DONE?!
+///////////////////////////////////////////// DONE
+//Filters the URL database based on user logged in or user logged off
 app.get("/urls", (req, res) => {
+  let filteredDatabase = {};
+
+  for (let x in urlDatabase) {
+    if (req.session.user_id !== undefined && req.session.user_id === urlDatabase[x].poster) {
+      filteredDatabase[x] = urlDatabase[x];
+    } else if (req.session.user_id === undefined) {
+      filteredDatabase = urlDatabase;
+      break;
+    }
+  }
+
   let templateVars = {
-    urls: urlDatabase,
+    urls: filteredDatabase,
     usersInfo: users,
     user_id: req.session.user_id
   };
+
   res.render("urls_index", templateVars);
+});
+////////////////////////////////////////////////// DONE
+//Adds a page to add a new long URL / Short URL to database
+//Only users can add new links
+app.get("/urls/new", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase,
+    shortURL: req.params.id,
+    usersInfo: users,
+    user_id: req.session.user_id
+  };
+
+  if (req.session.user_id === undefined) {
+    res.redirect('/urls');
+  } else {
+    res.render("urls_new", templateVars);
+  }
+});
+/////////////////////////////////////////////// DONE
+// Adds the new URL to the databasse with poster info / short URL / long URL
+app.post("/urls", (req, res) => {
+  let randomString = generateRandomString();
+  urlDatabase[randomString] = {
+    longURL: req.body.longURL,
+    poster: req.session.user_id
+  };
+  res.redirect('/urls');
+});
+//////////////////////////////////////////// Done
+//
+app.get("/u/:shortURL", (req, res) => {
+  if (Object.keys(urlDatabase).indexOf(req.params.shortURL) > -1) {
+    res.redirect(urlDatabase[req.params.shortURL].longURL);
+  } else {
+    res.redirect('/urls/new');
+  }
+
+});
+////////////////////////////////////////// NEEDS TO DO MORE STUFF
+app.post("/urls/:id", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase,
+    shortURL: req.params.id,
+    user_id: req.session.user_id,
+    usersInfo: users
+  };
+  res.render("urls_show", templateVars);
 });
 ////////////////////////////////// Needs a redirect if logged in, and split into GET AND POST
 app.post("/login", (req, res) => {
@@ -82,12 +149,11 @@ app.post("/login", (req, res) => {
     let i = 1;
     Object.keys(users).forEach(function(element) {
       if (users[element].email === req.body.email && bcrypt.compareSync(req.body.password, users[element].password) === true) {
-        req.session.user_id = users[element].email;
+        req.session.user_id = element;
         res.redirect('/urls');
         i = 0;
       }
       if (Object.keys(users).length === i) {
-        console.log('HERE!');
         tempVars.response = 'Not a valid username or password';
         res.render("urls_login", tempVars);
       }
@@ -106,62 +172,30 @@ app.post("/login", (req, res) => {
     res.render("urls_login", tempVars);
   }
 });
-//////////////////////////////////////////// DONE
-//Logout function, deletes user_id cookie (not all in case i want to do the
-//stetch goals) and redirects to /urls
-app.post("/logout", (req, res) => {
-  req.session.user_id = undefined;
-  res.redirect('/urls');
-});
-//////////////////////////////////////////// DONE
-//look at json information
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-////////////////////////////////////////////////// Needs a redirct to login
-//Adds a page to add a new long URL / Short URL to database
-//Only users can add new links
-app.get("/urls/new", (req, res) => {
-  let templateVars = {
-    urls: urlDatabase,
-    shortURL: req.params.id,
-    usersInfo: users,
-    user_id: req.session.user_id
-  };
-  res.render("urls_new", templateVars);
-});
-/////////////////////////////////////////////// Needs to be GET /urls/:id?
-// WORK NEEDED
-app.post("/urls/add", (req, res) => {
-  let randomString = generateRandomString();
-  urlDatabase[randomString] = {
-    longURL: req.body.longURL,
-    poster: req.session.user_id
-  };
-  console.log(urlDatabase);
-  res.redirect('/urls');
-});
 //////////////////////////////////////// Needs to be broken into get and post
 app.post("/register", (req, res) => {
   let tempVars = {
     response: ''
   };
 
+  //If statment checks if the email and password are defined from text input
   if (req.body.password != undefined && req.body.email != undefined && req.body.password != '') {
+    //forEach loop checks the object if the email has been used before and sends error message
     Object.keys(users).forEach(function(element) {
       if (users[element].email === req.body.email) {
         tempVars.response = 'That email has already been registered.';
         res.render("urls_reg", tempVars);
       }
     });
-    if (tempVars.response != 'That email has already been registered.') {
+    //If email has not been registered, this code will execute
+    if (tempVars.response === '') {
       let randomString = generateRandomString();
       users[randomString] = {
         id: randomString,
         email: req.body.email,
         password: bcrypt.hashSync(req.body.password, 10)
       };
-      req.session.user_id = users[randomString].email;
+      req.session.user_id = randomString;
       res.redirect('/urls');
     }
   } else if (req.body.email === '' && req.body.email === '') {
@@ -177,6 +211,13 @@ app.post("/register", (req, res) => {
   }
 
 });
+//////////////////////////////////////////// DONE
+//Logout function, deletes user_id cookie (not all in case i want to do the
+//stetch goals) and redirects to /urls
+app.post("/logout", (req, res) => {
+  req.session.user_id = undefined;
+  res.redirect('/urls');
+});
 ////////////////////////////////////////////////////// ADD SOME ERROR MESSAGES
 app.post("/urls/:id/delete", (req, res) => {
   if (Object.keys(urlDatabase).indexOf(req.params.id) > -1) {
@@ -190,29 +231,10 @@ app.post("/urls/:id/delete", (req, res) => {
 app.post("/urls/:id/update", (req, res) => {
   if (Object.keys(urlDatabase).indexOf(req.params.id) > -1) {
     urlDatabase[req.params.id].longURL = req.body.longURL;
-    res.redirect('http://localhost:8080/urls');
+    res.redirect('/urls');
   } else {
-    res.redirect('http://localhost:8080/urls');
+    res.redirect('/urls');
   }
-});
-////////////////////////////////////////// NEEDS TO DO MORE STUFF
-app.post("/urls/:id", (req, res) => {
-  let templateVars = {
-    urls: urlDatabase,
-    shortURL: req.params.id,
-    user_id: req.session.user_id,
-    usersInfo: users
-  };
-  res.render("urls_show", templateVars);
-});
-//////////////////////////////////////////// ADD ERROR MESSAGE ON ELSE
-app.get("/u/:shortURL", (req, res) => {
-  if (Object.keys(urlDatabase).indexOf(req.params.shortURL) > -1) {
-    res.redirect(urlDatabase[req.params.shortURL].longURL);
-  } else {
-    res.redirect('/urls/new');
-  }
-
 });
 ///////////////////////////////////////////////////
 //function that generates a 6 string of a random alphanumeric value
